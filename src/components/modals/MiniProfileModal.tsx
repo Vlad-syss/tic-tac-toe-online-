559f292186afe9b6fe44ab1368f0db9a1a12d416
Vlad <vladbashak80@gmail.com>
2025-03-09 15:23:53 +0100

    change values

--- src/components/modals/MiniProfileModal.tsx
+++ src/components/modals/MiniProfileModal.tsx
@@ -1,4 +1,4 @@
-import { CircleUser, PenLine } from 'lucide-react'
+import { CircleUser } from 'lucide-react'
 import { motion } from 'motion/react'
 import { FC } from 'react'
 import { LogoutButton } from '../LogoutButton'
@@ -45,29 +45,30 @@ export const MiniProfileModal: FC<MiniProfileModalProps> = ({
 				initial='hidden'
 				animate='visible'
 				exit='exit'
-				className='px-1 py-1 rounded-md bg-emerald-400 text-emerald-800 shadow-lg shadow-emerald-900/20 z-50 w-[160px] absolute mt-2 text-xs'
+				className='px-1 py-1 rounded-md bg-emerald-400 text-emerald-800 shadow-lg shadow-emerald-900/20 w-[160px] absolute mt-2 text-xs z-51'
 				style={{
 					top: position.y,
 					left: position.x + 40,
 				}}
+				onClick={e => e.stopPropagation()}
 			>
 				<ul className='flex flex-col'>
-					<li className='flex items-center justify-between py-2 px-1 rounded-sm rounded-b-none hover:bg-emerald-100/30 cursor-pointerv border-b'>
+					<a
+						href='/profile'
+						className='flex items-center justify-between py-2 px-1 rounded-sm rounded-b-none hover:bg-emerald-100/30 border-b'
+					>
 						<span className='text-gray-700'>View Profile</span>
 						<CircleUser className='text-gray-500 w-4 h-4' />
-					</li>
-					<li className='flex items-center justify-between py-2 px-1 rounded-sm rounded-b-none  hover:bg-emerald-100/30 cursor-pointer border-b'>
-						<span className='text-gray-700'>Edit Profile</span>
-						<PenLine className='text-gray-500 w-4 h-4' />
-					</li>
+					</a>
+
 					<li className='flex items-center justify-between py-2 px-1 rounded-b-none bg-emerald-900/20  hover:bg-emerald-100/30 cursor-pointer border-b'>
-						<label htmlFor='#logout' className='text-gray-900 font-semibold'>
+						<label
+							htmlFor='logout'
+							className='text-gray-900 font-semibold cursor-pointer'
+						>
 							Logout
 						</label>
-						<LogoutButton
-							id='logout'
-							className='w-auto h-auto bg-transparent hover:bg-transparent px-0 font-semibold'
-						/>
+						<LogoutButton className='w-auto h-auto bg-transparent hover:bg-transparent px-0 font-semibold' />
 					</li>
 				</ul>
 			</motion.div>
