1cef4b139133c2a81f1d5c930690222c00444fb8
Vlad <vladbashak80@gmail.com>
2025-03-13 20:22:11 +0100

    link components for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
-import { useAuth0 } from '@auth0/auth0-react'
+import { GameModes } from '../main/game'
+import { Leaderboard } from '../main/leaderboard'
 
 export function Home() {
-	const { user } = useAuth0()
-
 	return (
-		<div className='flex-[1_0_auto]'>
-			<p>Hello {user !== undefined && user.name}</p>
+		<div className='flex flex-col flex-[1_0_auto] items-center space-y-8 px-4 py-8'>
+			<h1 className='text-5xl font-bold text-green-900'>Tic-Tac-Toe Arena</h1>
+			<div className='gap-1 grid grid-cols-[1.5fr_1fr]'>
+				<GameModes />
+				<Leaderboard />
+			</div>
 		</div>
 	)
 }
