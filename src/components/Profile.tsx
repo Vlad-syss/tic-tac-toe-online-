14ef4616006dcb3ddc21b3fb55fb120c794779fb
Vlad <vladbashak80@gmail.com>
2025-03-25 20:30:34 +0100

    adapt for game profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,36 @@
-import { LoaderCircle, Settings2 } from 'lucide-react'
+import cn from 'classnames'
+import {
+	Circle,
+	LoaderCircle,
+	Settings2,
+	Square,
+	Triangle,
+	X,
+} from 'lucide-react'
 import { useRef, useState } from 'react'
+import { Id } from '../../convex/_generated/dataModel'
 import { useMiniProfileModal, useUser } from '../hooks'
+import { useGetUserById } from '../hooks/useUser'
+import { SymbolType } from '../types'
 import { Button } from './Button'
 import { MiniProfileModal } from './modals/MiniProfileModal'
+interface ProfileProps {
+	isGame?: boolean
+	userId?: Id<'users'>
+	isAi?: boolean
+	reverse?: boolean
+	userSymbol?: SymbolType | null
+}
 
-export const Profile = () => {
-	const { user, isLoading } = useUser()
+export const Profile = ({
+	isGame,
+	userId,
+	isAi,
+	reverse,
+	userSymbol,
+}: ProfileProps) => {
+	const { user, isLoading } =
+		isGame && userId ? useGetUserById(userId) : useUser()
 	const { isOpen, onClose, onOpen } = useMiniProfileModal()
 	const profileRef = useRef<HTMLDivElement>(null)
 	const [modalPosition, setModalPosition] = useState({ x: 0, y: 0 })
@@ -28,8 +53,19 @@ export const Profile = () => {
 	if (!user) return <p>User not found</p>
 	return (
 		<div className='relative flex gap-2 items-top group' ref={profileRef}>
-			<div className='flex gap-2 select-none'>
-				<a href='/profile' className='cursor-pointer'>
+			<div
+				className={cn(
+					'flex gap-2 select-none',
+					reverse && 'flex-row-reverse justify-end text-right'
+				)}
+			>
+				<a
+					href='/profile'
+					className={cn(
+						'cursor-pointer relative',
+						isGame && 'pointer-events-none'
+					)}
+				>
 					<img
 						src={user.avatarUrl?.split('=')[0] || './default-avatar.png'}
 						width={50}
@@ -37,34 +73,76 @@ export const Profile = () => {
 						alt='profile_img'
 						className='rounded border border-green-400'
 					/>
+					{userSymbol && (
+						<div
+							className={cn(
+								'absolute -top-1',
+								reverse ? '-left-1' : '-right-1'
+							)}
+						>
+							{userSymbol === 'O' && (
+								<Circle className='w-5 h-5 bg-white p-1 rounded-full text-blue-500' />
+							)}
+							{userSymbol === 'Square' && (
+								<Square className='w-5 h-5 bg-white p-1 rounded-full text-purple-500' />
+							)}
+							{userSymbol === 'Triangle' && (
+								<Triangle className='w-5 h-5 bg-white p-1 rounded-full text-orange-500' />
+							)}
+							{userSymbol === 'X' && (
+								<X className='w-5 h-5 bg-white p-1 rounded-full text-red-500' />
+							)}
+						</div>
+					)}
 				</a>
 
 				<div className='flex flex-col gap-0.5'>
 					<h4 className='text-sm truncate'>{user.name}</h4>
-					<p className='text-lime-800 text-xs leading-2'>
-						Rating online:{' '}
-						<span className='font-semibold'>{user.onlineRating}</span>
-					</p>
-					<p className='text-lime-800 text-xs leading-5'>
-						Rating offline:{' '}
-						<span className='font-semibold'>{user.onlineRating}</span>
-					</p>
+					{!isAi && isGame && (
+						<p className='text-lime-800 text-xs leading-2'>
+							Rating online:{' '}
+							<span className='font-semibold'>{user.onlineRating}</span>
+						</p>
+					)}
+					{isAi && isGame && (
+						<p className='text-lime-800 text-xs leading-2'>
+							Rating offline:{' '}
+							<span className='font-semibold'>{user.offlineRating}</span>
+						</p>
+					)}
+					{!isGame && (
+						<>
+							<p className='text-lime-800 text-xs leading-2'>
+								Rating online:{' '}
+								<span className='font-semibold'>{user.onlineRating}</span>
+							</p>
+							<p className='text-lime-800 text-xs leading-5'>
+								Rating offline:{' '}
+								<span className='font-semibold'>{user.offlineRating}</span>
+							</p>
+						</>
+					)}
 				</div>
 			</div>
-			<Button
-				size='costum'
-				variant='greenOutline'
-				className='h-5 w-5 p-0 border-lime-700 rounded-sm group-hover:bg-lime-400/40 hover:bg-lime-400/40 cursor-pointer'
-				onClick={handleOpenModal}
-			>
-				<Settings2 className='text-lime-700 h-3 w-3' />
-			</Button>
 
-			<MiniProfileModal
-				isOpen={isOpen}
-				onClose={onClose}
-				position={modalPosition}
-			/>
+			{!isGame && (
+				<Button
+					size='costum'
+					variant='greenOutline'
+					className='h-5 w-5 p-0 border-lime-700 rounded-sm group-hover:bg-lime-400/40 hover:bg-lime-400/40 cursor-pointer'
+					onClick={handleOpenModal}
+				>
+					<Settings2 className='text-lime-700 h-3 w-3' />
+				</Button>
+			)}
+
+			{!isGame && (
+				<MiniProfileModal
+					isOpen={isOpen}
+					onClose={onClose}
+					position={modalPosition}
+				/>
+			)}
 		</div>
 	)
 }
