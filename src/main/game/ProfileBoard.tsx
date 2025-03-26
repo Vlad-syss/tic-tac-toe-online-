2861b1d7a66d76b97768ea315b12cedd2ca14711
Vlad <vladbashak80@gmail.com>
2025-03-26 21:35:07 +0100

    change ui for profile board

--- src/main/game/ProfileBoard.tsx
+++ src/main/game/ProfileBoard.tsx
@@ -1,13 +1,14 @@
+import cn from 'classnames'
 import { FC } from 'react'
 import { Id } from '../../../convex/_generated/dataModel'
 import { Profile } from '../../components/Profile'
 import { SymbolType } from '../../types'
-
 interface ProfileBoardProps {
 	userIds: Id<'users'>[]
 	isAi: boolean
 	userSymbols: Record<string, SymbolType>
 }
+
 export const ProfileBoard: FC<ProfileBoardProps> = ({
 	userIds,
 	isAi,
@@ -15,20 +16,32 @@ export const ProfileBoard: FC<ProfileBoardProps> = ({
 }) => {
 	return (
 		<div className='pb-3 mb-3 border-b-2'>
-			<div className='flex justify-between bg-green-300/70 p-2 px-3 rounded-lg'>
+			<div className='flex relative justify-between bg-green-300/70 p-4 px-5 rounded-lg shadow-md border border-green-500'>
 				{userIds.map((userId, i) => {
 					const isReverse = (i + 1) % 2 === 0
 					const userSymbol = userSymbols ? userSymbols[userId] : null
 
 					return (
-						<Profile
+						<div
 							key={userId}
-							userId={userId}
-							isGame={true}
-							isAi={isAi}
-							reverse={isReverse}
-							userSymbol={userSymbol}
-						/>
+							className={cn(
+								'flex items-center gap-2',
+								isReverse ? 'flex-row' : 'flex-row-reverse'
+							)}
+						>
+							<div className='flex flex-col items-center justify-center w-24 h-12 pt-2 pb-1 border-2 border-emerald-400 rounded-lg shadow-lime-200/40 text-lg font-semibold text-green-700'>
+								<h3 className='text-sm text-gray-600 mr-2'>⏳ Timer:</h3>
+								<span className='text-green-700 font-bold'>01:00</span>
+							</div>
+							<div className='text-2xl text-emerald-800'>|</div>
+							<Profile
+								userId={userId}
+								isGame={true}
+								isAi={isAi}
+								reverse={isReverse}
+								userSymbol={userSymbol}
+							/>
+						</div>
 					)
 				})}
 			</div>
